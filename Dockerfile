9d6645c3f442d9442366d860d0a2c881bb20f0e9
Didac Cristobal <didac.crst@gmail.com>
2025-07-25 08:54:06 +0200

    Harmonize logging displays

--- Dockerfile
+++ Dockerfile
@@ -23,4 +23,4 @@ RUN ls -l ./src
 RUN python -m pip install --no-cache-dir --editable .
 
 EXPOSE 8000
-CMD ["uvicorn", "mockexchange_api.server:app", "--host", "0.0.0.0", "--port", "8000"]
+CMD ["uvicorn", "mockexchange_api.server:app", "--host", "0.0.0.0", "--port", "8000", "--log-config", "./src/mockexchange_api/log_cfg.json"]
--- src/mockexchange/logging_config.py
+++ src/mockexchange/logging_config.py
@@ -5,7 +5,8 @@ import sys
 logging.basicConfig(
     level=logging.INFO,                 # DEBUG for more verbosity
     stream=sys.stdout,                  # log to container stdout
-    format="%(asctime)s  %(levelname)s  %(name)s | %(message)s",
+    format="%(asctime)s | %(levelname)-8s | %(name)s | %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",          # ← no “,%f” ⇒ no milliseconds
 )
 
 logger = logging.getLogger(__name__)   # module-local logger
